6d1e8a688f319c308fb5312667c8045d93b85374
Karrar Abd Ali <>
2021-08-03 20:10:31 +0200

    Starting project init commit

---
+++ MovieFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFlix
+//
+//  Created by Karrar Abd Ali on 03/08/2021.
+//  Copyright © 2021 Karrar Abd Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
