9ac06515e7fd0c968368ed2ff4b73ed58cf1a60f
Karrar Abd Ali <>
2021-08-04 13:20:13 +0200

    Adding searching functionality local and API

--- MovieFlix/Controllers/HomeTableViewController.swift
+++ MovieFlix/Controllers/HomeTableViewController.swift
@@ -12,17 +12,21 @@ class HomeTableViewController: UIViewController {
     //MARK:- Var
     @IBOutlet weak var tableView: UITableView!
     private var movies: [Movie]? = []
+    @IBOutlet weak var searchBar: UISearchBar!
     private let homeTableViewCellIdentifier = "HomePageMovieTableViewCell"
+    
+    private var searchedMovies:[Movie] = []
+    private var isSearching: Bool = false
     
     //MARK:- Init
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupTableView()
+        setupSearchBar()
         loadData()
     }
-    
-    
+        
     //MARK: - Methods
     private func loadData() {
         APIClient.shared.getHomePageMovies(completion: { result in
@@ -31,6 +35,7 @@ class HomeTableViewController: UIViewController {
             case .success(let movieObject):
                 print(movieObject.results?.count ?? "Nil")
                 self.movies = movieObject.results
+                self.searchedMovies = movieObject.results ?? []
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -45,11 +50,19 @@ class HomeTableViewController: UIViewController {
         tableView.register(UINib(nibName: homeTableViewCellIdentifier, bundle: .main), forCellReuseIdentifier: homeTableViewCellIdentifier)
         tableView.separatorStyle = .none
     }
+    
+    private func setupSearchBar() {
+        searchBar.delegate = self
+    }
 }
 
 extension HomeTableViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        movies?.count ?? 0
+        if isSearching {
+            return searchedMovies.count
+        } else {
+        return movies?.count ?? 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -64,7 +77,8 @@ extension HomeTableViewController: UITableViewDelegate, UITableViewDataSource {
                 self.navigationController?.pushViewController(vc, animated: true)
             }
         }
-        guard  let movies = movies, cell != nil else {return UITableViewCell()}
+        let moviesArray = isSearching ? searchedMovies : self.movies
+        guard  let movies = moviesArray, cell != nil else {return UITableViewCell()}
         let movie = movies[indexPath.row]
         cell?.infoButtonAction = action
         if let posterPath = movie.posterPath {
@@ -92,3 +106,39 @@ extension HomeTableViewController: UITableViewDelegate, UITableViewDataSource {
     
 }
 
+extension HomeTableViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let movies = (self.movies != nil) ? self.movies! : []
+        isSearching = !searchText.isEmpty
+        if searchText.isEmpty {
+            searchBar.resignFirstResponder()
+        }
+        searchedMovies = movies.filter({
+            if let searched =  $0.originalTitle?.prefix(searchText.count), searched.lowercased() == searchText.lowercased() {
+                return true
+            }
+            return false
+        })
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.searchTextField.text, !text.isEmpty else {
+            isSearching = false
+            return
+        }
+        
+        isSearching = true
+            APIClient.shared.searchMovie(name: text, completion: { result in
+                switch result {
+                case .success(let movieObject):
+                    self.movies?.append(contentsOf: movieObject.results ?? [])
+                    self.searchedMovies.append(contentsOf: movieObject.results ?? [])
+                    DispatchQueue.main.async {self.tableView.reloadData()}
+                case .failure(let error): print(error)
+                self.isSearching = false
+                DispatchQueue.main.async {self.tableView.reloadData()}
+                }
+            })
+    }
+}
